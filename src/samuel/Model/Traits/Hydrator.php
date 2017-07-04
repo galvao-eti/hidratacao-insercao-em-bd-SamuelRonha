4f60b7a61259fd18623dec08598a8132f87ad4ca
samue <samuelronha@gmail.com>
2017-07-04 20:53:27 -0300

    finalização de classe de hidratação e remoção de autoload.

--- src/samuel/Model/Traits/Hydrator.php
+++ src/samuel/Model/Traits/Hydrator.php
@@ -1,21 +1,20 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
- * User: samue
+ * User: Samuel
  * Date: 03/07/2017
  * Time: 21:04
  */
-
-namespace Model\Traits;
-
-
 trait Hydrator
 {
-    public function hydrator($objects){
-        $class_vars = get_class_vars(get_class($objects));
-
-        foreach ($class_vars as $name => $value) {
-            echo "$name : $value\n";
+    public function hydrator($objects)
+    {
+        $class_vars = array_keys(get_class_vars(__CLASS__));
+        foreach ($objects as $name => $value) {
+            if (in_array($name, $class_vars)) {
+                $this->{$name} = $value;
+            }
         }
     }
 }
